852ee81abef62994120f9827b9b68909493cb746
artem_muntyanu <a.muntyanu@triplenext.com>
2024-10-08 14:37:07 +0100

    feat: better responsiveness

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export function Header() {
     return (
         <header className="fixed left-0 top-0 z-50 w-full border-b backdrop-blur-[12px]">
-            <div className="flex min-h-[3.5rem] max-w-[1000px] mx-auto px-4 items-center justify-between">
+            <div className="flex min-h-[3.5rem] max-w-screen-lg mx-auto px-4 items-center justify-between">
                 <Link href="/" className="inline-flex mr-[10px]">
                     <Image src="/logo.svg" alt="Datopus" width={0} height={0}  style={{ 'width': 'auto', 'height': '2rem' }} />
                 </Link>
