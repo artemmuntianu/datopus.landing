972ea35113a8b4eab3e8210ca5655e1da6fc7dc4
artem_muntyanu <a.muntyanu@triplenext.com>
2024-10-25 17:53:56 +0100

    feat: added chat widget tawk.to

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,6 +60,20 @@ export default function RootLayout({
 }>) {
     return (
         <html lang='en' suppressHydrationWarning>
+            <head>
+                <script suppressHydrationWarning dangerouslySetInnerHTML={{
+                    __html: `
+                        var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+                        (function () {
+                            var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+                            s1.async = true;
+                            s1.src = 'https://embed.tawk.to/671bbda22480f5b4f593ccb2/1ib25phaq';
+                            s1.charset = 'UTF-8';
+                            s1.setAttribute('crossorigin', '*');
+                            s0.parentNode.insertBefore(s1, s0);
+                        })();`
+                }} />
+            </head>
             <CSPostHogProvider>
                 <body
                     className={cn(
